82bf7bec1c07d231b8985ea216b2144fa1790023
FilipMijic <email@example.com>
2024-09-11 15:14:30 +0200

    Added filters on home page to view games, adjusted method for adding games and finished get games method

--- src/app/games/games.page.ts
+++ src/app/games/games.page.ts
@@ -3,6 +3,7 @@ import { Game } from './game.model';
 import { GamesService } from './games.service';
 import { ModalController } from '@ionic/angular';
 import { GameModalComponent } from './game-modal/game-modal.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-games',
@@ -12,10 +13,31 @@ import { GameModalComponent } from './game-modal/game-modal.component';
 export class GamesPage implements OnInit {
 
   games: Game[] = [];
+  filteredGames: Game[] = [];
+  private gamesSub: Subscription;
 
   constructor(private gamesService: GamesService, private modalCtrl: ModalController) {}
 
   ngOnInit() {
+    this.gamesSub = this.gamesService.games.subscribe((games) => {
+    this.filteredGames = [...this.games];
+  });
+
+  }
+
+  ionViewWillEnter() {
+    this.gamesService.getGames().subscribe((games) => {
+    this.filteredGames = [...this.games];
+    });
+  }
+
+  filterGames(event: any) {
+    const status = event.detail.value;
+    if (status === 'all') {
+      this.filteredGames = [...this.games];
+    } else {
+      this.filteredGames = this.games.filter(game => game.status === status);
+    }
   }
 
   openModal() {
--- src/app/games/games.service.ts
+++ src/app/games/games.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, switchMap, take, tap } from 'rxjs';
 import { Game } from './game.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -28,14 +28,40 @@ export class GamesService {
   }
 
   addGame(title: string, developer: string, publisher: string, genre: string, platform: string, status: string, imageUrl: string) {
+    
+    let generatedId;
     let newGame: Game;
 
     newGame = new Game(null, title, developer, genre, publisher, platform, status, imageUrl);
-    return this.http.post<{ name: string }>(`https://personal-game-library-app-default-rtdb.europe-west1.firebasedatabase.app/games.json`, newGame);
+    return this.http.post<{ name: string }>(`https://personal-game-library-app-default-rtdb.europe-west1.firebasedatabase.app/games.json`, newGame)
+    .pipe(switchMap((resData) => {
+      generatedId = resData.name;
+      return this.games;
+    }), 
+    take(1), 
+    tap((games => {
+      newGame.id = generatedId;
+      this._games.next(games.concat(newGame))
+    })))
   }
 
-  getGames(id: string) {
-    return this.http.get<{ [key: string]: GameData }>(`https://book-app-fon-nmr-default-rtdb.europe-west1.firebasedatabase.app/books.json`);
+  getGames() {
+    return this.http.get<{ [key: string]: GameData }>(`https://book-app-fon-nmr-default-rtdb.europe-west1.firebasedatabase.app/books.json`)
+    .pipe(map((gamesData) => {
+      const games: Game[] = [];
+        for (const key in gamesData) {
+          if (gamesData.hasOwnProperty(key)) {
+            games.push(new Game(key, gamesData[key].title, gamesData[key].developer, gamesData[key].genre, gamesData[key].publisher, gamesData[key].platform, gamesData[key].status, gamesData[key].imageUrl));
+          }
+        }
+        this._games.next(games);
+        return games;
+    }),
+    tap(games => {
+      this._games.next(games);
+    })
+    );
+    
   }
 
   getGame(id: string) {
