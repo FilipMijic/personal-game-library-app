0a965ef91e00ce28e0c89f3211ddd6fcea80eb62
FilipMijic <email@example.com>
2024-09-14 11:22:04 +0200

    Application now builds in android studio with ionic capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'personal-game-library-app',
+  appId: 'app-library-game-personal',
+  appName: 'Personal game library',
   webDir: 'www'
 };
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: "AIzaSyDqgOYCaDaI1341FM4EHJ1fRtrzv5rL8dA"
 };
